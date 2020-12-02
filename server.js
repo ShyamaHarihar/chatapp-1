b25aa243e97b1ba7fff8e31e4fb98e24c09fc6a7
Shyama Harihar <59372076+ShyamaHarihar@users.noreply.github.com>
2020-12-02 22:32:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,12 @@
 const express = require('express')
 const app=express()
 const http=require('http').createServer(app)
+ 
 
-const PORT=process.env.PORT || 3000
-//defined below for heroku
-app.listen(process.env.PORT || 3000);
-http.listen(PORT,()=>{
-    console.log(`Listening on port ${PORT}`)
-})
+
+app.listen((process.env.PORT || 3000), function () {
+  console.log("Server started on port 3000");
+});
 app.use(express.static(__dirname+'/public'))
 app.get('/',(req,res)=>{
     res.sendFile(__dirname+'/index.html')
